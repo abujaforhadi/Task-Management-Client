b07409427a57facc19a84331367fb1a05523e97c
abujaforhadi <abujaforhadi@gmail.com>
2025-02-20 14:17:13 +0600

    auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 
 
+import { RouterProvider } from 'react-router'
 import './App.css'
+import { router } from './router/Router'
 
 function App() {
 
   return (
     <>
-     
+     <RouterProvider router={router} />
       
     </>
   )
